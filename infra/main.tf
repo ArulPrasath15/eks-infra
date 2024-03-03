23630271bb451f931804186d500aea7e50e94c5d
Arul Prasath <arulprasath73@outlook.com>
2024-03-03 20:49:26 +0530

    added ignore

--- infra/main.tf
+++ infra/main.tf
@@ -54,10 +54,10 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  tags =  {
-      username = "arulprasathv"
-      purpose  = "learning"
-    }
+  tags = {
+    username = "arulprasathv"
+    purpose  = "learning"
+  }
 }
 
 ################################################################################
