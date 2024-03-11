30b2c623ce3348e50c2a6a987a6edad810d98984
Arul Prasath <arulprasath73@outlook.com>
2024-03-11 22:35:15 +0530

    added manifest

---
+++ infra/var.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "arul-kube"
+}
+
+variable "node_group_name" {
+  type = string
+  default = "arul-compute-power"
+}
